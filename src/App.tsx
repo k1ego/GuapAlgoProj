66bbd343a907bb2f2a46e4631821365875a988d5
k1ego <ladonin05@mail.ru>
2024-12-08 21:12:23 +0300

    All dependencies are installed and the project is configured without redax

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import { Button } from "@nextui-org/react";
 
 function App() {
-
-  return (
-    <>
-      <div>Hello world</div>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<Button color='primary'>Button</Button>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
